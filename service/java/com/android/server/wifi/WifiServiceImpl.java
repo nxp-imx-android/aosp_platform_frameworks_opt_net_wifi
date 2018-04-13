ee30ea04784d6f83d73df88c951cac936f39618a
yang.tian <yang.tian@nxp.com>
2018-04-13 11:51:32 +0800

    MA-11634[Android_AUTO]WIFI disabled automatically when reboot system
    For Android AUTO, we make wifi driver installed as module after boot
    completed, which caused wifi service start without driver loaded.
    
    Register boot complete broadcast receiver for wifi service, and trigger
    wifi enabled on boot completed broadcase received.
    
    Change-Id: I206ded8cbc1c57bcef14fd05b1fa4cee794a7c3f
    Signed-off-by: Tian Yang<yang.tian@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/4403
    Reviewed-by: guoyin.chen <guoyin.chen@nxp.com>
    Reviewed-by: Wang Haoran <elven.wang@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/4535
    Reviewed-by: zhang bo <bo.zhang@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/5380
    Reviewed-on: http://androidsource.ap.freescale.net/project/9896

--- service/java/com/android/server/wifi/WifiServiceImpl.java
+++ service/java/com/android/server/wifi/WifiServiceImpl.java
@@ -520,6 +520,8 @@ public class WifiServiceImpl extends BaseWifiService {
         Slog.i(TAG, "WifiService starting up with Wi-Fi " +
                 (wifiEnabled ? "enabled" : "disabled"));
 
+        boolean wifiStateStored = wifiEnabled;
+
         registerForScanModeChange();
         mContext.registerReceiver(
                 new BroadcastReceiver() {
@@ -566,6 +568,24 @@ public class WifiServiceImpl extends BaseWifiService {
                     }
                 },
                 new IntentFilter(WifiManager.WIFI_AP_STATE_CHANGED_ACTION));
+
+        mContext.registerReceiver(
+                new BroadcastReceiver() {
+                    @Override
+                    public void onReceive(Context context, Intent intent) {
+                        try {
+                            Thread.sleep(5000);
+                        }catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        Log.d(TAG, "boot completed do start wifi\n");
+
+                        if (wifiStateStored) {
+                            setWifiEnabled(mContext.getPackageName(), wifiStateStored);
+                        }
+                    }
+                },
+                new IntentFilter(Intent.ACTION_BOOT_COMPLETED));
 
         // Adding optimizations of only receiving broadcasts when wifi is enabled
         // can result in race conditions when apps toggle wifi in the background
