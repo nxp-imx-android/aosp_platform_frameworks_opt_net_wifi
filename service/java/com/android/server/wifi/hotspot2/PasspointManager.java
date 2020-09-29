bcdf42cf230deb562602c6dbc997b67e3bcb48c8
Nate Jiang <qiangjiang@google.com>
2020-09-29 11:11:32 -0700

    Update WifiConfigManager when autojoin changed
    Bug: 168573596
    Test: atest com.android.server.wifi
    Change-Id: I5041dc70ee4cffeb18da25986ce1e68b06db6149
    (cherry picked from commit db5df1e401b8ed84762d8697a6ccd9e40778292c)

--- service/java/com/android/server/wifi/hotspot2/PasspointManager.java
+++ service/java/com/android/server/wifi/hotspot2/PasspointManager.java
@@ -596,7 +596,11 @@ public class PasspointManager {
                                 ? UserActionEvent.EVENT_CONFIGURE_AUTO_CONNECT_ON
                                 : UserActionEvent.EVENT_CONFIGURE_AUTO_CONNECT_OFF,
                         provider.isFromSuggestion(), true);
+                // Update WifiConfigManager if changed.
+                updateWifiConfigInWcmIfPresent(provider.getWifiConfig(), provider.getCreatorUid(),
+                        provider.getPackageName(), provider.isFromSuggestion());
             }
+
             mWifiConfigManager.saveToStore(true);
             return true;
         }
@@ -612,6 +616,10 @@ public class PasspointManager {
                                     ? UserActionEvent.EVENT_CONFIGURE_AUTO_CONNECT_ON
                                     : UserActionEvent.EVENT_CONFIGURE_AUTO_CONNECT_OFF,
                             provider.isFromSuggestion(), true);
+                    // Update WifiConfigManager if changed.
+                    updateWifiConfigInWcmIfPresent(provider.getWifiConfig(),
+                            provider.getCreatorUid(), provider.getPackageName(),
+                            provider.isFromSuggestion());
                 }
                 found = true;
             }
